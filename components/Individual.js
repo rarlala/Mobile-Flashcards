14b3147ea06f7079854ffb4bf0d22667d215a269
JEONGSUJI <wtw9611@gmail.com>
2020-04-20 21:36:12 +0900

    Individual View sample styling

---
+++ components/Individual.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { getDesksInfo } from '../utils/helpers';
+
+class DeskList extends Component {
+  addCard({}) {}
+  startQuiz({}) {}
+
+  render() {
+    const key = 'one';
+    const desk = getDesksInfo(key);
+
+    return (
+      <View style={styles.container}>
+        <Text style={styles.title}>{desk['title']}</Text>
+        <Text style={styles.card}>
+          {Object.keys(desk['questions']).length} cards
+        </Text>
+        <TouchableOpacity
+          onPress={this.addCard}
+          style={[styles.button, styles.addCard]}
+        >
+          <Text>Add Card</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={this.addCard}
+          style={[styles.button, styles.startQuiz]}
+        >
+          <Text style={{ color: 'white' }}>Start Quiz</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+  title: {
+    marginBottom: 20,
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  card: {
+    marginBottom: 100,
+    fontSize: 15,
+    color: 'gray',
+  },
+  button: {
+    width: 200,
+    height: 60,
+    marginBottom: 15,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  addCard: {
+    borderColor: 'black',
+    borderStyle: 'solid',
+    borderWidth: 1,
+  },
+  startQuiz: {
+    backgroundColor: 'black',
+  },
+});
+
+export default DeskList;
