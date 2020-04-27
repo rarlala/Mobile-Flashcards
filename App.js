ad4542e6410fa3397d9b4b3e3afd5e8b05fbee38
JEONGSUJI <wtw9611@gmail.com>
2020-04-27 23:55:21 +0900

    Deck 추가 / reducer, actions 폴더 및 파일 생성

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import DeskList from './components/DeskList';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+
+import NewDeck from './components/NewDeck';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <DeskList />
-    </View>
+    <Provider store={createStore(reducer)}>
+      <View style={styles.container}>
+        <NewDeck />
+      </View>
+    </Provider>
   );
 }
 
