9f0b3a029f56fa6ea447b4baf30f1cf102c83748
JEONGSUJI <wtw9611@gmail.com>
2020-05-11 02:58:00 +0900

    임시 네비게이션  붙여보기

--- App.js
+++ App.js
@@ -1,16 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Platform, View, StatusBar } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
 
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
 import DeskList from './components/DeskList';
+import AddCard from './components/AddCard';
+import NewDeck from './components/NewDeck';
+import Quiz from './components/Quiz';
+import Individual from './components/Individual';
+
+const Tabs =
+  Platform.OS === 'ios'
+    ? createBottomTabNavigator()
+    : createMaterialTopTabNavigator();
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={styles.container}>
-        <DeskList />
+        <NavigationContainer>
+          <Tabs.Navigator initialRouteName="DeckList">
+            <Tabs.Screen name="DeckList" component={DeskList} />
+            <Tabs.Screen name="AddCard" component={AddCard} />
+            <Tabs.Screen name="NewDeck" component={NewDeck} />
+            <Tabs.Screen name="Quiz" component={Quiz} />
+            <Tabs.Screen name="Individual" component={Individual} />
+          </Tabs.Navigator>
+        </NavigationContainer>
       </View>
     </Provider>
   );
